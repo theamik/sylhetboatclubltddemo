fb18baab3fe15ce8d7e256e26fb9de098efcdaa0
theamik <amik.sylhet@gmail.com>
2026-06-04 17:32:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="page">
+      <div className="card">
+        <div className="logo">⚓</div>
+
+        <h1>Sylhet Boat Club Ltd</h1>
+
+        <div className="line"></div>
+
+        <h2>Under Construction</h2>
+
+        <p className="developer">
+          Developing by{" "}
+          <a href="https://www.softmariyam.com" target="_blank">
+            SoftMariyam
+          </a>
+          <br />
+          <span>www.softmariyam.com</span>
+        </p>
+
+        <p className="phone">📞 01719-948515</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
